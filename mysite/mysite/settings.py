8f80c13e62ead907f10fddbc8f4e28081acbd0f3
unknown <yx.zhang@fulltech-metal.com>
2018-01-15 10:09:37 +0800

    init 2018-01-15

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e2!^*@-*1u^=8t$2vh_t1khiysa_jawqhp18sumf$9-f(a@5zv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.10.0.111','192.168.62.23','test2018-001-twoutlook.c9users.io']
+ALLOWED_HOSTS = ['10.10.0.111','192.168.62.11','test2018-001-twoutlook.c9users.io']
 
 
 # Application definition
